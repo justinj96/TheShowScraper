dca34211cf4afb239d08d4c3d253191b98903cf5
Justin Jacobson <justinjacobson@Justins-MacBook-Pro-2.local>
2020-04-16 17:05:00 -0700

    added rarity

--- theshowscraper.py
+++ theshowscraper.py
@@ -4,12 +4,19 @@ import re
 import webbrowser
 import sys
 
-marketPage = sys.argv[1]
+if len(sys.argv) != 3:
+    print(f"Missing Command Line Args")
+    print(f"Usage: python3 theshowscraper.py <rarity> <page number>")
+    print(f"Rarity: Common = 1, Bronze = 2, etc")
+    sys.exit(0)
+
+cardRarity = sys.argv[1]
+marketPage = sys.argv[2]
 
 marketLink = ""
 
 try:
-    marketLink = "https://theshownation.com/mlb20/community_market?page=" + marketPage + "&display_position=&amp=&max_best_buy_price=&max_best_sell_price=&max_rank=&min_best_buy_price=&min_best_sell_price=&min_rank=&name=&player_type_id=&rarity_id=1&series_id=&team_id=&type=mlb_card"
+    marketLink = f'https://theshownation.com/mlb20/community_market?page={marketPage}&display_position=&amp=&max_best_buy_price=&max_best_sell_price=&max_rank=&min_best_buy_price=&min_best_sell_price=&min_rank=&name=&player_type_id=&rarity_id={cardRarity}&series_id=&team_id=&type=mlb_card'
 except:
     print("Invalid Page Number")
 
@@ -24,7 +31,5 @@ for link in soup.find_all('a', href=True):
         if link['href'] not in links:
             links.append(link['href'])
 
-print(len(links))
-
-# for link in links:
-#     webbrowser.get('chrome').open_new_tab("https://theshownation.com/" + link)
+for link in links:
+    webbrowser.get('chrome').open_new_tab(f"https://theshownation.com/{link}")
